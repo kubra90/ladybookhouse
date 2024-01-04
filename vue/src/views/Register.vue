038867537b8fe9fee56329326015aebc71930c60
Kubrianity <kubraaozerenn@gmail.com>
2024-01-04 07:57:29 +0300

    Update style for login and register views

--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -1,33 +1,43 @@
 <template>
-  <div id="register" class="text-center">
-    <form @submit.prevent="register">
-      <h1>Create Account</h1>
-      <div role="alert" v-if="registrationErrors">
-        {{ registrationErrorMsg }}
-      </div>
-      <div class="form-input-group">
-        <label for="username">Username</label>
-        <input type="text" id="username" v-model="user.username" required autofocus />
-      </div>
-      <div class="form-input-group">
-        <label for="password">Password</label>
-        <input type="password" id="password" v-model="user.password" required />
-      </div>
-      <div class="form-input-group">
-        <label for="confirmPassword">Confirm Password</label>
-        <input type="password" id="confirmPassword" v-model="user.confirmPassword" required />
-      </div>
-      <button type="submit">Create Account</button>
-      <p><router-link :to="{ name: 'login' }">Already have an account? Log in.</router-link></p>
-    </form>
-  </div>
+  <main>
+    <header-page/>
+    <div id="register">
+      <form @submit.prevent="register">
+        <h1>Create Account</h1>
+        <div role="alert" v-if="registrationErrors">
+          {{ registrationErrorMsg }}
+        </div>
+        <div class="form-input-group">
+          <label for="username">Username</label>
+          <input type="text" id="username" v-model="user.username" required autofocus />
+        </div>
+        <div class="form-input-group">
+          <label for="password">Password</label>
+          <input type="password" id="password" v-model="user.password" required autocomplete="on" />
+        </div>
+        <div class="form-input-group">
+          <label for="confirmPassword">Confirm Password</label>
+          <input type="password" id="confirmPassword" v-model="user.confirmPassword" required autocomplete="on" />
+        </div>
+        <button type="submit">Create Account</button>
+        <p><router-link :to="{ name: 'login' }">Already have an account? Log in.</router-link></p>
+      </form>
+    </div>
+    <footer-page class="footer"/>
+  </main>
 </template>
 
 <script>
 import authService from '../services/AuthService';
+import HeaderPage from "../components/HeaderPage.vue"
+import FooterPage from "../components/FooterPage.vue"
 
 export default {
   name: 'register',
+  components: {
+    HeaderPage,
+    FooterPage
+  },
   data() {
     return {
       user: {
@@ -74,10 +84,53 @@ export default {
 </script>
 
 <style scoped>
+#register {
+  margin: 0;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+  text-align: center;
+}
+form h1 {
+  font-family: 'PT Sans', sans-serif;
+  font-size: 30px;
+  font-style: normal;
+  line-height: 1.25;
+  text-align: center;
+}
 .form-input-group {
-  margin-bottom: 1rem;
+  margin: 1rem;
+  padding: 0.75rem;
 }
 label {
-  margin-right: 0.5rem;
+  margin-right: 0.75rem;
+}
+label[for=confirmPassword] {
+  margin-left: -4em;
+}
+button {
+  cursor: pointer;
+  border: 0;
+  border-radius: 4px;
+  font-weight: 600;
+  width: 120px;
+  padding: 5px 0;
+  margin-bottom: 5px;
+  box-shadow: 0 0 20px rgba(104, 85, 224, 0.2);
+  transition: 0.4s;
+  color: #337ab7;
+  background-color: rgba(255, 255, 255, 1);
+  border: 1px solid rgba(104, 85, 224, 1);
+}
+button:hover {
+  color: white;
+  box-shadow: 0 0 5px #337ab7;
+  background-color: #337ab7;
+}
+.footer {
+  position:absolute;
+  bottom: 0;
 }
 </style>
