1288889a4f59299ce15aea6d6a54dc2ee1b535e6
kubra <kubracetin.ist@gmail.com>
2024-01-15 15:05:43 -0500

    contact page

---
+++ vue/src/components/ContactPage.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <p class="contact-header">Contact Us</p>
+        <p><strong>Lady Bookhouse</strong></p>
+        <p>375 Acorn Park Dr. 1310</p>
+        <p>Belmont, MA, 02478</p>
+    </div>
+</template>
+<script>
+</script>
+<style scoped>
+
+</style>
