fe676629ebaa36714dd2ae10779907295a154ca5
kubra <kubracetin.ist@gmail.com>
2024-01-15 14:58:29 -0500

    added shipping cost logic and contact

--- vue/src/components/ShoppingCart.vue
+++ vue/src/components/ShoppingCart.vue
@@ -27,16 +27,9 @@
           </div>
        </div>
        <div class="price-tab">
-<<<<<<< HEAD
-        <p class="subtotal-price">Subtotal <span>${{formatPrice(totalPrice)}}</span> </p>
-        <p class="shipping-price">Shipping <span>{{ totalShippingCost }}</span></p>
-||||||| bc774ba
-        <p class="subtotal-price">Subtotal <span>${{formatPrice(totalPrice)}}</span> </p>
-=======
         <p class="subtotal-price">Subtotal <span>${{formatPrice(subTotalPrice)}}</span> </p>
         <p class="shipping-price">Shipping <span>${{formatPrice(totalShippingCost) }}</span> </p>
->>>>>>> kubratpn
-        <p class="total-price">Order Total <span class="order-total"> ${{ formatPrice(totalPrice)}}</span></p>
+        <p class="total-price">Order Total <span> ${{ formatPrice(totalPrice)}}</span></p>
        </div>
        <div>
         <router-link v-bind:to="{name: 'checkout'}">
@@ -78,6 +71,14 @@ export default {
                 shippingCost =5.65;
             }else if(totalWeight>48 && totalWeight<=64){
                 shippingCost =6.40;
+            }else if(totalWeight>64 && totalWeight<=80){
+                shippingCost =7.15;
+            }else if(totalWeight>80 && totalWeight <=96){
+                shippingCost = 7.90;
+            }else if(totalWeight>96 && totalWeight<=112){
+                shippingCost = 8.65;
+            }else{
+                shippingCost =9.99;
             }
             //implement more shopping cost here.
             return shippingCost;
@@ -245,12 +246,16 @@ flex-direction: row;
     padding: 0px 0; /* Add some padding */
 }
 
-.subtotal-price, .total-price {
+.subtotal-price, .total-price, .shipping-price {
     margin-bottom: 0px; /* Reduce space between subtotal and total */
 }
-.total-price span, .subtotal-price span {
+.total-price span, .subtotal-price span{
     
     margin-left: 20px;
+}
+
+.shipping-price span{
+    margin-left:27px;
 }
 .total-price span{
     color:chocolate;
