fc5912597ca9adeaa6c8c81da961d7415f99d46c
kevser özeren <ozerenkevser92@gmail.com>
2024-03-01 23:09:17 +0300

    Apple style to browse links

--- vue/src/views/Browse.vue
+++ vue/src/views/Browse.vue
@@ -1,33 +1,63 @@
 <template>
- <div>
-  <header-page />
-  <ul>
-   <router-link :to="{ path:`/${category}` }" v-for="category in categories" :key="category" ><p>{{ category }}</p></router-link>
-  </ul>
-  <footer-page />
- </div>
+  <div>
+    <header-page />
+    <div class="links">
+      <router-link
+        :to="{ path: `/${category}` }"
+        v-for="category in categories"
+        :key="category"
+      >
+        <div class="link">{{ category }}</div>
+      </router-link>
+    </div>
+    <footer-page />
+  </div>
 </template>
-    
+
 <script>
-    
 import HeaderPage from "../components/HeaderPage.vue"
 import FooterPage from "../components/FooterPage.vue"
 
 export default {
   name: "browse",
-    
+
   data() {
     return {
-     categories: ["History&Memory", "Science", 
-     "Other", "Bestseller", "Religion&Philosophy", 
-     "Literature", "Social Science", "Cookbook", "Kids", 
-     "Design", "Technology", "Art", "Language"
-    ]
-  }  
-},
+      categories: [
+        "History&Memory",
+        "Science",
+        "Other",
+        "Bestseller",
+        "Religion&Philosophy",
+        "Literature",
+        "Social Science",
+        "Cookbook",
+        "Kids",
+        "Design",
+        "Technology",
+        "Art",
+        "Language",
+      ]
+    }
+  },
   components: {
-    HeaderPage, FooterPage
-  }
-    
+    HeaderPage,
+    FooterPage,
+  },
 }
 </script>
+
+<style scoped>
+.links {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  max-width: 100%;
+  padding-inline-start: 12rem;
+}
+.link {
+  display: inline-block;
+  margin: 1rem;
+  padding: 0.2rem;
+  text-align: center;
+}
+</style>
