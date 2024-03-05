2d60451d0d00f3b860c32e454a1a67d9c2f88e61
kubra <kubracetin.ist@gmail.com>
2024-03-05 16:26:37 -0500

    implementing bootstrap 5

--- vue/src/components/AdvancedSearch.vue
+++ vue/src/components/AdvancedSearch.vue
@@ -4,67 +4,61 @@
     <h2>Advanced Search</h2>
     <form class="form-horizontal">
         <!-- Categories -->
-        <div class="form-group">
-            <label for="categorySelect" class="col-sm-1 control-label">Search in:</label>
-            <div class="col-sm-4">
-                <select v-model="searchForm.category" class="form-control" id="categorySelect">
-                    <option value="">All Categories</option>
-                    <!-- Populate categories here -->
-                    <option v-for="category in categories" :key="category.value">
-                    {{ category.text }}
-                    </option>
-                    
-                </select>
-            </div>
+        <div class="mb-4 row"> <!-- Categories -->
+          <label for="categorySelect" class="col-form-label col-sm-2">Search in:</label>
+          <div class="col-sm-8">
+            <select v-model="searchForm.category" class="form-select" id="categorySelect">
+              <option value="">All Categories</option>
+              <option v-for="category in categories" :key="category.value" :value="category.value">
+                {{ category.text }}
+              </option>
+            </select>
+          </div>
         </div>
         
-        <!-- Author -->
-        <div class="form-group">
-            <label for="authorInput" class="col-sm-1 control-label">Author:</label>
-            <div class="col-sm-4">
-                <input v-model="searchForm.author" type="text"  class="form-control" id="authorInput" placeholder="Author Name">
-            </div>
+        <div class="mb-4 row"> <!-- Author -->
+          <label for="authorInput" class="col-form-label col-sm-2">Author:</label>
+          <div class="col-sm-8">
+            <input v-model="searchForm.author" type="text" class="form-control" id="authorInput" placeholder="Author Name">
+          </div>
         </div>
 
-        <!-- Title -->
-        <div class="form-group">
-            <label for="titleInput" class="col-sm-1 control-label">Title:</label>
-            <div class="col-sm-4">
-                <input v-model="searchForm.title" type="text" class="form-control" id="titleInput" placeholder="Book Title">
-            </div>
+        <div class="mb-4 row"> <!-- Title -->
+          <label for="titleInput" class="col-form-label col-sm-2">Title:</label>
+          <div class="col-sm-8">
+            <input v-model="searchForm.title" type="text" class="form-control" id="titleInput" placeholder="Book Title">
+          </div>
         </div>
 
-        <!-- Keywords Option -->
-        <div class="form-group">
-            <label for="keywordInput" class="col-sm-1 control-label">Keywords:</label>
-            <div class="col-sm-4">
-                <input v-model="searchForm.keywords" type="text" class="form-control" id="keywordOption" placeholder="Keywords">
-            </div>
+        <div class="mb-4 row"> <!-- Keywords Option -->
+          <label for="keywordInput" class="col-form-label col-sm-2">Keywords:</label>
+          <div class="col-sm-8">
+            <input v-model="searchForm.keywords" type="text" class="form-control" id="keywordInput" placeholder="Keywords">
+          </div>
         </div>
 
-         <!-- Price Option -->
-         <div class="form-group">
-            <label for="priceInput" class="col-sm-1 control-label">Price:</label>
-            <div class="col-sm-2">
-                <div class="input-group">
-                <span class="input-group-addon">$</span>
-                <input v-model="searchForm.minPrice" type="text" class="form-control" id="priceOption" placeholder="Min Range">
+        <div class="mb-3 row"> <!-- Price Option -->
+          <label for="minPriceInput" class="col-form-label col-sm-2">Price:</label>
+          <div class="col-sm-4">
+            <div class="input-group">
+              <span class="input-group-text">$</span>
+              <input v-model="searchForm.minPrice" type="text" class="form-control" id="minPriceInput" placeholder="Min Range">
             </div>
-        </div>
-            <div class="col-sm-2">
-                <div class="input-group">
-                <span class="input-group-addon">$</span>
-                <input v-model="searchForm.maxPrice" type="text" class="form-control" id="priceOption" placeholder="Max Range">
+          </div>
+          <div class="col-sm-4">
+            <div class="input-group">
+              <span class="input-group-text">$</span>
+              <input v-model="searchForm.maxPrice" type="text" class="form-control" id="maxPriceInput" placeholder="Max Range">
             </div>
-            </div>
+          </div>
         </div>
 
-        <!-- Search Button -->
-        <div class="form-group">
-            <div class="col-sm-offset-1 col-sm-10">
-                <button type="submit" class="btn btn-primary" @click.prevent="searchBookDetails">Search</button>
-            </div>
+        <div class="mb-5 row"> <!-- Search Button -->
+          <div class="offset-sm-2 col-sm-10">
+            <button type="submit" class="btn btn-primary" @click.prevent="searchBookDetails">Search</button>
+          </div>
         </div>
+     
     </form>
 </div>
 
