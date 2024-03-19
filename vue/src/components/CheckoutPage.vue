f3022e81230a41062ee3f7d21f0f8111519bc0fa
kubra <kubracetin.ist@gmail.com>
2024-03-18 23:08:17 -0400

    added options for shipping form

--- vue/src/components/CheckoutPage.vue
+++ vue/src/components/CheckoutPage.vue
@@ -93,10 +93,10 @@
                     id="flexCheckDefault"
                   />
                   <label class="form-check-label" for="flexCheckDefault"
-                    >Keep me up to date on news</label
+                    >Keep me up to date on new arrivals and discount</label
                   >
                 </div>
-                <!-- new  -->
+            
               </div>
             </div>
 
@@ -118,8 +118,8 @@
                           checked
                         />
                         <label class="form-check-label" for="flexRadioDefault1">
-                          Express delivery <br />
-                          <small class="text-muted">3-4 days via Fedex </small>
+                            USPS Media <br />
+                          <small class="text-muted">6-11 business days</small>
                         </label>
                       </div>
                     </div>
@@ -135,15 +135,15 @@
                           id="flexRadioDefault2"
                         />
                         <label class="form-check-label" for="flexRadioDefault2">
-                          Post office <br />
-                          <small class="text-muted">20-30 days via post </small>
+                          UPS Express <br />
+                          <small class="text-muted">4-7 business days </small>
                         </label>
                       </div>
                     </div>
                   </div>
                   <div class="col-lg-4 mb-3">
                     <!-- Default radio -->
-                    <div class="form-check h-100 border rounded-3">
+                    <!-- <div class="form-check h-100 border rounded-3">
                       <div class="p-3">
                         <input
                           class="form-check-input"
@@ -156,7 +156,7 @@
                           <small class="text-muted">Come to our shop </small>
                         </label>
                       </div>
-                    </div>
+                    </div> -->
                   </div>
                 </div>
 
@@ -174,11 +174,61 @@
                   </div>
 
                   <div class="col-sm-4 mb-3">
-                    <p class="mb-0">City</p>
+                    <p class="mb-0">State</p>
                     <select class="form-select">
-                      <option value="1">New York</option>
-                      <option value="2">Moscow</option>
-                      <option value="3">Samarqand</option>
+                        <option value="">N/A</option>
+			<option value="AK">Alaska</option>
+			<option value="AL">Alabama</option>
+			<option value="AR">Arkansas</option>
+			<option value="AZ">Arizona</option>
+			<option value="CA">California</option>
+			<option value="CO">Colorado</option>
+			<option value="CT">Connecticut</option>
+			<option value="DC">District of Columbia</option>
+			<option value="DE">Delaware</option>
+			<option value="FL">Florida</option>
+			<option value="GA">Georgia</option>
+			<option value="HI">Hawaii</option>
+			<option value="IA">Iowa</option>
+			<option value="ID">Idaho</option>
+			<option value="IL">Illinois</option>
+			<option value="IN">Indiana</option>
+			<option value="KS">Kansas</option>
+			<option value="KY">Kentucky</option>
+			<option value="LA">Louisiana</option>
+			<option value="MA">Massachusetts</option>
+			<option value="MD">Maryland</option>
+			<option value="ME">Maine</option>
+			<option value="MI">Michigan</option>
+			<option value="MN">Minnesota</option>
+			<option value="MO">Missouri</option>
+			<option value="MS">Mississippi</option>
+			<option value="MT">Montana</option>
+			<option value="NC">North Carolina</option>
+			<option value="ND">North Dakota</option>
+			<option value="NE">Nebraska</option>
+			<option value="NH">New Hampshire</option>
+			<option value="NJ">New Jersey</option>
+			<option value="NM">New Mexico</option>
+			<option value="NV">Nevada</option>
+			<option value="NY">New York</option>
+			<option value="OH">Ohio</option>
+			<option value="OK">Oklahoma</option>
+			<option value="OR">Oregon</option>
+			<option value="PA">Pennsylvania</option>
+			<option value="PR">Puerto Rico</option>
+			<option value="RI">Rhode Island</option>
+			<option value="SC">South Carolina</option>
+			<option value="SD">South Dakota</option>
+			<option value="TN">Tennessee</option>
+			<option value="TX">Texas</option>
+			<option value="UT">Utah</option>
+			<option value="VA">Virginia</option>
+			<option value="VT">Vermont</option>
+			<option value="WA">Washington</option>
+			<option value="WI">Wisconsin</option>
+			<option value="WV">West Virginia</option>
+			<option value="WY">Wyoming</option>
                     </select>
                   </div>
 
