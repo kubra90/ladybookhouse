e2c5ee68c37241cd483640c1d03fb07906c324e7
kubra <kubracetin.ist@gmail.com>
2024-04-30 15:46:48 -0400

    bootstrap5

--- vue/src/components/ContactPage.vue
+++ vue/src/components/ContactPage.vue
@@ -1,53 +1,33 @@
 <template>
-  <div class="contact-container">
-    <p class="contact-header">Contact Us</p>
-    <div class="contact-address">
-    <p><strong>Lady Bookhouse</strong></p>
-    <p>375 Acorn Park Dr. 1310</p>
-    <p>Belmont, MA 02478</p>
+  <div class="container my-4">
+    <h1 class="mb-4" style="color: brown; font-family: 'PT Sans', sans-serif;">Contact Us</h1>
+    <div class="row">
+      <div class="col-md-6">
+        <div class="card border-0">
+          <div class="card-body">
+            <p class="card-title fw-bold">Lady Bookhouse</p>
+            <p class="card-text">375 Acorn Park Dr. 1310</p>
+            <p class="card-text">Belmont, MA 02478</p>
+            <p class="mt-3">
+              <strong>Mail:</strong> 
+              <a href="mailto:ladybookhouse@gmail.com" class="text-decoration-none" style="color: brown;">ladybookhouse@gmail.com</a>
+            </p>
+          </div>
+        </div>
       </div>
-    <p class="mail-address"><strong>Mail: </strong><a class="mail-address-link" href="mailto:ladybookhouse@gmail.com">ladybookhouse@gmail.com</a></p>
+    </div>
   </div>
 </template>
-<script>
-</script>
+
 
 <style scoped>
-.contact-header {
-  margin-top: 4%;
-  margin-left: 9%;
-  margin-bottom:4%;
-  font-size:39px;
-  color:brown;
-  font-family: "PT Sans", sans-serif;
+/* You might still want to keep some custom styles */
+h1 {
+  margin-top: 2%; /* Optional, as Bootstrap's margins may suffice */
+  font-size: 39px; /* Large font size for the header */
 }
 
-.contact-address {
-  margin-left:9%;
-  margin-top:2%;
-  margin-bottom: 3%;
-  display: flex;
-  flex-direction: column;
-}
-
-.contact-address p {
-  margin: 0; /* Removes default margin */
-  padding: 0; /* Optional: if you want to remove default padding as well */
-}
-
-.mail-address{
-  margin-left: 9%;
-  margin-bottom:4%;
-}
-
-.contact-container {
-  display: flex;
-  flex-direction: column;
-  position:relative;
-
-}
-
-.mail-address-link{
-  color:brown;
+a {
+  color: brown; /* Specific color for links */
 }
 </style>
